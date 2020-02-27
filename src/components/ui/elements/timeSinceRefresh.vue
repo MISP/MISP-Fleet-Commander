746d885b8ede82747adc4bae8393bbe219424d70
mokaddem <sami.mokaddem@circl.lu>
2020-02-27 17:07:09 +0100

    chg: [component:timeSinceRefresh] Highlight if critical

--- src/components/ui/elements/timeSinceRefresh.vue
+++ src/components/ui/elements/timeSinceRefresh.vue
@@ -1,5 +1,5 @@
 <template>
-    <span class="mr-1 text-muted" style="cursor: auto;">
+    <span :class="['mr-1', moreThanOneDay ? 'text-danger' : 'text-muted']" style="cursor: auto;">
         <template v-if="validTimestamp !== false">
             <i class="far fa-clock mr-1"></i>
             <small class="align-middle">
@@ -22,6 +22,9 @@ export default {
     computed: {
         validTimestamp() {
             return Number.isInteger(this.timestamp) ? this.timestamp : false
+        },
+        moreThanOneDay() {
+            return (new Date()).getTime()/1000 - this.timestamp > 3600
         }
     }
 }
