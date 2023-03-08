5f984d448ef6c88cbf50f10b371842a504812adf
Sami Mokaddem <mokaddem.sami@gmail.com>
2023-03-08 09:52:18 +0100

    chg: [backend:instance] Added support of serving vue application through flask

--- backend/application/controllers/instance.py
+++ backend/application/controllers/instance.py
@@ -1,9 +1,24 @@
-from flask import Blueprint, request, render_template, make_response, jsonify
+from flask import Blueprint, request, render_template, make_response, jsonify, send_from_directory
 import application.models.servers as serverModel
 from application.DBModels import db, Server
+import os.path
 
 
 BPinstance = Blueprint('instance', __name__)
+
+
+@BPinstance.route('/', defaults={'path': ''})
+@BPinstance.route('/<string:path>')
+@BPinstance.route('/<path:path>')
+def static_proxy(path):
+    if os.path.isfile('./dist/' + path):
+        # If request is made for a file by vue.js for example main.js
+        # condition will be true, file will be served from the public directory
+        return send_from_directory('../dist', path)
+    else:
+        # Otherwise index.html will be served,
+        # vue.js router will handle the rest
+        return send_from_directory('../dist', "index.html")
 
 
 @BPinstance.route('/instance/searchAll/<searchtext>', methods=['GET'])
