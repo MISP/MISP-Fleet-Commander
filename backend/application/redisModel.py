9de31e51b5332e93602d0bc508f00b0b19bd6f5d
Sami Mokaddem <mokaddem.sami@gmail.com>
2023-06-29 08:59:25 +0200

    new: [backend:server_info] Added TLL

--- backend/application/redisModel.py
+++ backend/application/redisModel.py
@@ -4,11 +4,14 @@ from typing import Union
 from application import redisClient
 
 KEY_SERVER_INFO = 'server-info'
+SERVER_INFO_TTL = 60*60
 
 
 def saveServerInfo(server_uuid: int, info: dict) -> bool:
     infoText = json.dumps(info)
-    return redisClient.set(f"{KEY_SERVER_INFO}:{server_uuid}", infoText)
+    saved = redisClient.set(f"{KEY_SERVER_INFO}:{server_uuid}", infoText)
+    redisClient.expire(f"{KEY_SERVER_INFO}:{server_uuid}", SERVER_INFO_TTL)
+    return saved
 
 def getServerInfo(server_uuid: str) -> Union[None, dict]:
     infoText = redisClient.get(f"{KEY_SERVER_INFO}:{server_uuid}")
