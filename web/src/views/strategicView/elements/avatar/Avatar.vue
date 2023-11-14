158053e6768c845a60e15b9945ec1efd93766570
Sami Mokaddem <mokaddem.sami@gmail.com>
2023-11-14 14:03:13 +0100

    chg: [web:strategicView] Added missing avatar view files

---
+++ web/src/views/strategicView/elements/avatar/Avatar.vue
@@ -0,0 +1,63 @@
+<template>
+    <span>
+        <span class="m-1 event-avatar" v-if="pinlist_model == 'event'">
+            <AvatarImage v-bind="$attrs" :avatar_id="avatar_id"></AvatarImage>
+        </span>
+        <span class="m-1 sharinggroup-avatar" v-else-if="pinlist_model == 'sharinggroup'">
+            <AvatarImage v-bind="$attrs" :avatar_id="avatar_id"></AvatarImage>
+        </span>
+        <span class="m-1 sighting-avatar" v-else-if="pinlist_model == 'sighting'">
+            <AvatarImage v-bind="$attrs" :avatar_id="avatar_id"></AvatarImage>
+        </span>
+    </span>
+</template>
+
+<script>
+import AvatarImage from "@/views/strategicView/elements/avatar/AvatarImage.vue"
+
+
+export default {
+    name: "Avatar",
+    inheritAttrs: false,
+    components: {
+        AvatarImage
+    },
+    props: {
+        avatar_id: {
+            type: Number,
+            required: true,
+        },
+        pinlist_model: {
+            type: String,
+            required: true,
+        },
+    },
+    data: function () {
+        return {}
+    },
+    computed: {
+    },
+    methods: {
+    },
+}
+</script>
+
+<style scoped>
+.event-avatar {
+    display: inline-block;
+    box-shadow: 0 0 4px 0 #00000077;
+}
+
+.sharinggroup-avatar {
+    display: inline-block;
+    border: 2px solid #2a009d;
+}
+
+.sighting-avatar {
+    display: inline-block;
+    border: 1px solid #00000066;
+    border-radius: 50%;
+    overflow: hidden;
+}
+
+</style>
---
+++ web/src/views/strategicView/elements/avatar/AvatarImage.vue
@@ -0,0 +1,40 @@
+<template>
+    <span>
+        <img
+            :src="`${avatarURL}/${avatar_id}.png`"
+            v-bind="$attrs"
+            class="" alt="Entry avatar"
+        >
+    </span>
+</template>
+
+<script>
+import pinlistsApi from "@/api/pinLists"
+
+
+export default {
+    name: "AvatarImage",
+    inheritAttrs: false,
+    props: {
+        avatar_id: {
+            type: Number,
+            required: true,
+        }
+    },
+    data: function () {
+        return {}
+    },
+    computed: {
+        avatarURL() {
+            const url = pinlistsApi.avatarURL()
+            return url
+        }
+    },
+    methods: {
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
