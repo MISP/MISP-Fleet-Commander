72bc7465fb0a4b161b43112c569355c2602c9c7e
Sami Mokaddem <mokaddem.sami@gmail.com>
2023-11-15 10:29:46 +0100

    chg: [backend:start] Onlyt start necessary service by default

--- backend/startall.sh
+++ backend/startall.sh
@@ -5,6 +5,6 @@ sleep 0.1
 screen -S "mispsync-server" -X screen -t "flask" bash -c "bash start.sh"
 screen -S "mispsync-server" -X screen -t "redis" bash -c "redis-server"
 screen -S "mispsync-server" -X screen -t "celery-worker" bash -c ". ./venv/bin/activate && celery -A application.celery worker -l INFO"
-screen -S "mispsync-server" -X screen -t "flower" bash -c ". ./venv/bin/activate && celery -A application.celery flower --port=5566"
+# screen -S "mispsync-server" -X screen -t "flower" bash -c ". ./venv/bin/activate && celery -A application.celery flower --port=5566"
 # screen -S "mispsync-server" -X screen -t "sqlite_admin" bash -c ". ./venv/bin/activate && sqlite_web --host 127.0.0.1 --port 8088 app.db"
 
