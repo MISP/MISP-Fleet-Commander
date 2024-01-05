791b9e6a650c6c0a5a037af57239186e73bae672
Sami Mokaddem <mokaddem.sami@gmail.com>
2024-01-05 12:01:12 +0100

    fix: [web:connectionSummary] MAke sure to display all valid connection even when expand_issue_only is enabled

--- web/src/views/servers/elements/connectionsSummary.vue
+++ web/src/views/servers/elements/connectionsSummary.vue
@@ -143,7 +143,7 @@ export default {
     },
     computed: {
         hasValidConnections() {
-            return this.connections !== undefined && this.getConnectionList.length > 0 && this.getConnectionList[0]['error'] === undefined
+            return this.connections !== undefined && this.getAllConnectionsList.length > 0 && this.getAllConnectionsList[0]['error'] === undefined
         },
         connectionCount() {
             return this.getConnectionList.length
