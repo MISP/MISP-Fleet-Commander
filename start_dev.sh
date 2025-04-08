5fe10a57cc444cb606738e7130f2b32780d6938a
Sami Mokaddem <mokaddem.sami@gmail.com>
2025-04-08 15:15:59 +0200

    chg: [backend] Added start_dev script

--- start_dev.sh
+++ start_dev.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 pushd backend
-bash startall.sh
+bash startall_debug.sh
 popd
 pushd web
 screen -S "mispsync-server" -X screen -t "web" bash -c "cd ../web && npm run serve; read x;"
