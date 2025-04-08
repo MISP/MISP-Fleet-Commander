acfb4cff2719f71898db70f6f2910758d3105bc7
Sami Mokaddem <mokaddem.sami@gmail.com>
2025-04-08 10:57:45 +0200

    chg: [app:build] Cleaned-up build script

--- build_web.sh
+++ build_web.sh
@@ -5,11 +5,8 @@ set -o errexit
 script_dir=$(dirname "$(realpath "$0")")
 cd "$script_dir" || exit 1
 
-rm -f backend/application/dist.zip 
 rm -rf backend/application/dist
 pushd web
 npm run buildproduction
 popd
-pushd backend/application/
-zip -r dist.zip dist
 
